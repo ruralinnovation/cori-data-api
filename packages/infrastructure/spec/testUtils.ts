3b66a1bc013fab743759c506f0f22c7baca1e282
John Hall <john.hall@ruralinnovation.us>
2023-05-17 13:05:48 -0400

    simplify test configuration and update readme

--- packages/infrastructure/spec/testUtils.ts
+++ packages/infrastructure/spec/testUtils.ts
@@ -4,7 +4,19 @@ import { getConfig, getLocalGitBranch, TestEnvConfig } from '../../../config';
 require('dotenv').config();
 
 export const getTestConfig = async (): Promise<TestEnvConfig> => {
-  if (process.env.GIT_BRANCH) {
+
+  console.log("CONFIFURING TEST ENVIRONMENT");
+  console.log("process.env.GIT_BRANCH: ", process.env.GIT_BRANCH);
+  console.log({
+    username: process.env.TEST_USER!,
+    password: process.env.TEST_PASSWORD!,
+
+    region: process.env.AWS_REGION!,
+    userPoolId: process.env.USER_POOL_ID!,
+    apiUrl: process.env.API_URL!,
+    cognitoClientId: process.env.COGNITO_CLIENT_ID!,
+  });
+  if (!!process.env.GIT_BRANCH) {
     console.log('Running in CICD, use existing environment vars');
     const cfg = {
       username: process.env.TEST_USER!,
@@ -22,12 +34,18 @@ export const getTestConfig = async (): Promise<TestEnvConfig> => {
   const branch = await getLocalGitBranch();
   const config = getConfig(branch);
 
+  console.log('config', {
+    ...config.testing,
+    username: process.env.INTEGRATION_TESTING_USERNAME!,
+    password: process.env.INTEGRATION_TESTING_PASSWORD!,
+  });
+
+
   if (!config.testing) {
     throw new Error('Testing endpoint is undefined');
   }
 
-  console.log('config', config.testing);
-  // * @todo: Should clean this up and move it to .dotenv file or something
+  // * @TODO: Should clean this up and move it to .dotenv file or something
   return {
     ...config.testing,
     username: process.env.INTEGRATION_TESTING_USERNAME!,
