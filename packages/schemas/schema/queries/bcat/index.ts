fb0bf80dd49def3b03722b2c02fd9277737bb57c
John Hall <john.hall@ruralinnovation.us>
2023-05-19 15:33:22 -0400

    updates for main

--- packages/schemas/schema/queries/bcat/index.ts
+++ packages/schemas/schema/queries/bcat/index.ts
@@ -4,6 +4,7 @@
 import county_summary from "./county_summary";
 import county_summary_geojson from "./county_summary_geojson";
 import auction_904_subsidy_awards_geojson from "./auction_904_subsidy_awards_geojson";
+import auction_904_authorized from "./auction_904_authorized";
 import auction_904_authorized_geojson from "./auction_904_authorized_geojson";
 import auction_904_ready_geojson from "./auction_904_ready_geojson";
 import auction_904_defaults_geojson from "./auction_904_defaults_geojson";
@@ -11,6 +12,7 @@ import auction_904_defaults_geojson from "./auction_904_defaults_geojson";
 const bcatQueries: any = {
   county_summary: county_summary,
   county_summary_geojson: county_summary_geojson,
+  auction_904_authorized: auction_904_authorized,
   auction_904_authorized_geojson: auction_904_authorized_geojson,
   auction_904_ready_geojson: auction_904_ready_geojson,
   auction_904_defaults_geojson: auction_904_defaults_geojson,
