eae8b88c26a8a48324daab522f310ef6d8afd404
John Hall <john.hall@ruralinnovation.us>
2023-06-07 10:20:19 -0400

    add ready to authorize and defaults to graphql schema

--- packages/schemas/schema/queries/bcat/index.ts
+++ packages/schemas/schema/queries/bcat/index.ts
@@ -6,17 +6,21 @@ import county_summary_geojson from "./county_summary_geojson";
 import auction_904_subsidy_awards_geojson from "./auction_904_subsidy_awards_geojson";
 import auction_904_authorized from "./auction_904_authorized";
 import auction_904_authorized_geojson from "./auction_904_authorized_geojson";
+import auction_904_ready from "./auction_904_ready";
 import auction_904_ready_geojson from "./auction_904_ready_geojson";
+import auction_904_defaults from "./auction_904_defaults";
 import auction_904_defaults_geojson from "./auction_904_defaults_geojson";
 
 const bcatQueries: any = {
-  county_summary: county_summary,
-  county_summary_geojson: county_summary_geojson,
-  auction_904_authorized: auction_904_authorized,
-  auction_904_authorized_geojson: auction_904_authorized_geojson,
-  auction_904_ready_geojson: auction_904_ready_geojson,
-  auction_904_defaults_geojson: auction_904_defaults_geojson,
-  auction_904_subsidy_awards_geojson: auction_904_subsidy_awards_geojson,
+  county_summary,
+  county_summary_geojson,
+  auction_904_authorized,
+  auction_904_authorized_geojson,
+  auction_904_ready,
+  auction_904_ready_geojson,
+  auction_904_defaults,
+  auction_904_defaults_geojson,
+  auction_904_subsidy_awards_geojson
 };
 
 export default bcatQueries;
