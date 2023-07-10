8113a0e75446d824073d1b98ec41aa3be8a9b5b8
John Hall <john.hall@ruralinnovation.us>
2023-07-10 01:36:50 -0400

    connect humanity graphql resolver connects directly to pg

--- packages/schemas/schema/queries/connect-humanity/ch_test.ts
+++ packages/schemas/schema/queries/connect-humanity/ch_test.ts
@@ -1,29 +1,38 @@
-import { GraphQLFloat, GraphQLNonNull, GraphQLObjectType, GraphQLScalarType, GraphQLString } from "graphql/index";
-import { specifiedScalarTypes } from "graphql/type/scalars";
-import { GraphQLInt } from "graphql/type";
+import { GraphQLFloat, GraphQLObjectType, GraphQLString } from "graphql/index";
+// import { GraphQLList } from "graphql";
+import { QueryTypes, Sequelize } from "sequelize";
 
 const ch_test = {
-  type: new GraphQLObjectType({
-    name: 'CHTestObject',
-    fields: () => ({
-      "geoid_co": { type: GraphQLString },
-      "message": { type: GraphQLString },
-      "name": { type: GraphQLString },
-      "value": { type: GraphQLFloat },
-      "category": { type: GraphQLString },
-      "variable": { type: GraphQLString },
-      "category_pl": { type: GraphQLString },
-      "description": { type: GraphQLString },
-    })
-  }),
+  // An array (list) fails to transfer gzipped by local sam cli
+  type: // new GraphQLList(
+  // ... but a single return object can be gzipped locally
+    new GraphQLObjectType({
+      name: 'CHTestObject',
+      fields: () => ({
+        "geoid_co": { type: GraphQLString },
+        "geoid_tr": { type: GraphQLString },
+        "message": { type: GraphQLString },
+        "name": { type: GraphQLString },
+        "value": { type: GraphQLFloat },
+        "category": { type: GraphQLString },
+        "variable": { type: GraphQLString },
+        "category_pl": { type: GraphQLString },
+        "description": { type: GraphQLString },
+      })
+    }),
+  // ),
   args: null,
   resolve: async (
     _: any,
     __: any,
-    { dataSources: { pythonApi } }: any,
+    { dataSources: { dbConfiguration } }: any,
     info: any
-  // ) =>  await pythonApi.getItem(`acs/testing`);
   ) =>  {
+    console.log("Can now access dbConfiguration.getDBConfig...",  typeof dbConfiguration.getDBConfig);
+    const config = dbConfiguration.getDBConfig('proj_connect_humanity')[process.env.NODE_ENV || "development"];
+
+    const sequelize = new Sequelize(config.database, config.user, config.password, config);
+
     const value = {
       "geoid_co": "33009",
       "message": ("value of a top level property"),
@@ -35,9 +44,29 @@ const ch_test = {
       "description": "Percent of broadband serviceable locations with access to 25/3",
     };
 
-    // console.log("acs_test resolver will return value: ", value);
+    console.log("Resolve ch_tets no args: \n", __);
 
-    return value;
+    const values = await sequelize.query(`
+SELECT geoid, geoid_tr, CONCAT(statefp, countyfp) as geoid_co, pct_bb_25_3, st_simplify(st_transform(geom, 4326), 0.0) as geom
+    FROM sch_census_tiger.source_tiger_2020_tracts, sch_proj_climate.ch_app_wide_tract
+    WHERE statefp = '33' AND countyfp = '009' AND geoid = geoid_tr;
+`,
+      { type: QueryTypes.SELECT });
+
+    return values.map(v => {
+      console.log("ch_test resolver will return value: ", {
+        ...value,
+        ...v
+      });
+      console.log("pct_bb_25_3", v["pct_bb_25_3"]);
+
+      return {
+        ...value,
+        ...v,
+        "value": v["pct_bb_25_3"]
+      };
+    })      // An array (list) fails to transfer gzipped by local sam cli
+      [0];  // ... but a single return object can be gzipped locally
   }
 };
 
