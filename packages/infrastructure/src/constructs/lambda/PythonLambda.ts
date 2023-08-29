e20cd39334941074e986ab8ab5d9e96f0e3e15b4
John Hall <john.hall@ruralinnovation.us>
2023-08-29 12:10:10 -0400

    save before cdk update

--- packages/infrastructure/src/constructs/lambda/PythonLambda.ts
+++ packages/infrastructure/src/constructs/lambda/PythonLambda.ts
@@ -2,6 +2,7 @@ import { Construct } from 'constructs';
 import { RemovalPolicy } from 'aws-cdk-lib';
 import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { PythonFunction, PythonFunctionProps } from '@aws-cdk/aws-lambda-python-alpha';
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 export interface PythonLambdaProps extends PythonFunctionProps {
   /**
@@ -22,6 +23,7 @@ export class PythonLambda extends Construct {
 
     this.function = new PythonFunction(this, 'Function', {
       ...props,
+      runtime: Runtime.PYTHON_3_8
     });
 
     new LogGroup(this, 'LogGroup', {
