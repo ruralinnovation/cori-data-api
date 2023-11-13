6457dfe457f0cdb90ed3eb5cbef295f6967a3bf1
John Hall <john.hall@ruralinnovation.us>
2023-11-13 15:54:52 -0500

    update config for api dev

--- config/configs.ts
+++ config/configs.ts
@@ -49,12 +49,12 @@ export interface IMixedConfig extends ApiStackProps {
  * [NOTE]: You must have a valid service in the directory noted.
  */
 const microservicesConfiguration: ServiceConfig[] = [
-  {
-    /* Test/Trial of Python REST service */
-    logicalName: 'ACSService',
-    corePath: '/acs',
-    directoryName: 'acs',
-  },
+  // {
+  //   /* Test/Trial of Python REST service */
+  //   logicalName: 'ACSService',
+  //   corePath: '/acs',
+  //   directoryName: 'acs',
+  // },
   {
     /* Used by Broadband County Assessment Tool */
     logicalName: 'BCATService',
@@ -76,46 +76,46 @@ interface IConfigs {
   [name: string]: IMixedConfig;
 }
 
-const mfDefaults: Omit<IMixedConfig, 'client' | 'stage'> = {
-  project: 'data-api',
-  loggingLevel: 'info',
-  retain: false,
-  env: {
-    account: '190686435752',
-    region: 'us-east-1',
-  },
-  repo: 'mergingfutures/cori-data-api',
-  databaseConfig: {
-    vpcId: 'vpc-0499b35a2f5231aae',
-    databaseSecurityGroupId: 'sg-0be66ca1818bcc0e0',
-    host: 'cori-database-small-dev.c0no2rvbbm4n.us-east-1.rds.amazonaws.com',
-    dbname: 'data',
-    parameterName: '/cori/read_only_user_credentials',
-    dbuser: 'read_only_user',
-  },
-  cacheEnabled: true,
-  cacheConfig: {
-    host: 'redis-17358.c273.us-east-1-2.ec2.cloud.redislabs.com',
-    port: 17358,
-    username: 'default',
-    parameterName: '/cori/redis-cluster-credentials',
-    globalTTL: '86400',
-  },
-  microservicesConfig: microservicesConfiguration,
-  /**
-   * @todo: create a bucket with a prettier name
-   */
-  //artifactBucketName: 'coridataapicicdstack-devpipelineartifactsbucketfd-1smu59goaufdm',
-  //coridataapicicdstack-devpipelineartifactsbucketfd-1smu59goaufdm
-  testing: {
-    username: '/cori/int-test-user-name',
-    password: '/cori/int-test-user-pw',
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_NE91zaapX',
-    apiUrl: 'https://d25ssrwsq4u9bu.cloudfront.net',
-    cognitoClientId: '6um99fv2qtb6f7ise3i037vna',
-  },
-};
+// const mfDefaults: Omit<IMixedConfig, 'client' | 'stage'> = {
+//   project: 'data-api',
+//   loggingLevel: 'info',
+//   retain: false,
+//   env: {
+//     account: '190686435752',
+//     region: 'us-east-1',
+//   },
+//   repo: 'mergingfutures/cori-data-api',
+//   databaseConfig: {
+//     vpcId: 'vpc-0499b35a2f5231aae',
+//     databaseSecurityGroupId: 'sg-0be66ca1818bcc0e0',
+//     host: 'cori-database-small-dev.c0no2rvbbm4n.us-east-1.rds.amazonaws.com',
+//     dbname: 'data',
+//     parameterName: '/cori/read_only_user_credentials',
+//     dbuser: 'read_only_user',
+//   },
+//   cacheEnabled: true,
+//   cacheConfig: {
+//     host: 'redis-17358.c273.us-east-1-2.ec2.cloud.redislabs.com',
+//     port: 17358,
+//     username: 'default',
+//     parameterName: '/cori/redis-cluster-credentials',
+//     globalTTL: '86400',
+//   },
+//   microservicesConfig: microservicesConfiguration,
+//   /**
+//    * @todo: create a bucket with a prettier name
+//    */
+//   //artifactBucketName: 'coridataapicicdstack-devpipelineartifactsbucketfd-1smu59goaufdm',
+//   //coridataapicicdstack-devpipelineartifactsbucketfd-1smu59goaufdm
+//   testing: {
+//     username: '/cori/int-test-user-name',
+//     password: '/cori/int-test-user-pw',
+//     region: 'us-east-1',
+//     userPoolId: 'us-east-1_NE91zaapX',
+//     apiUrl: 'https://d25ssrwsq4u9bu.cloudfront.net',
+//     cognitoClientId: '6um99fv2qtb6f7ise3i037vna',
+//   },
+// };
 
 const coriDefaults: Omit<IMixedConfig, 'client' | 'stage'> = {
   project: 'data-api',
@@ -171,9 +171,12 @@ export const Config: IConfigs = {
     ...coriDefaults,
     client: 'cori',
     databaseConfig: {
-      ...coriDefaults.databaseConfig,
-      dbname: 'data',
+      dbname: 'api-dev',
       dbuser: 'read_only_user',
+      host: 'cori-risi-ad-postgresql.c6zaibvi9wyg.us-east-1.rds.amazonaws.com',
+      parameterName: '/postgresql/read_only_user_credentials',
+      databaseSecurityGroupId: 'sg-01ddcc192d814136f',
+      vpcId: 'vpc-08f5e17f5b75ccee9',
     },
     stage: 'dev'
   },
@@ -181,9 +184,12 @@ export const Config: IConfigs = {
     ...coriDefaults,
     client: 'cori',
     databaseConfig: {
-      ...coriDefaults.databaseConfig,
-      dbname: 'data',
+      dbname: 'api-dev',
       dbuser: 'read_only_user',
+      host: 'cori-risi-ad-postgresql.c6zaibvi9wyg.us-east-1.rds.amazonaws.com',
+      parameterName: '/postgresql/read_only_user_credentials',
+      databaseSecurityGroupId: 'sg-01ddcc192d814136f',
+      vpcId: 'vpc-08f5e17f5b75ccee9',
     },
     stage: 'dev'
   },
@@ -191,9 +197,12 @@ export const Config: IConfigs = {
     ...coriDefaults,
     client: 'cori',
     databaseConfig: {
-      ...coriDefaults.databaseConfig,
-      dbname: 'data',
+      dbname: 'api-dev',
       dbuser: 'read_only_user',
+      host: 'cori-risi-ad-postgresql.c6zaibvi9wyg.us-east-1.rds.amazonaws.com',
+      parameterName: '/postgresql/read_only_user_credentials',
+      databaseSecurityGroupId: 'sg-01ddcc192d814136f',
+      vpcId: 'vpc-08f5e17f5b75ccee9',
     },
     stage: 'local',
     testing: {
