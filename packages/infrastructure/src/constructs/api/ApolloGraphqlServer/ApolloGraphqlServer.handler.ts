aa80409371ca8a2b43f6268af91e5b0bec50902b
John Hall <john.hall@ruralinnovation.us>
2024-01-12 10:26:03 -0500

    fix required express and middleware dependencies in graphql server handler

--- packages/infrastructure/src/constructs/api/ApolloGraphqlServer/ApolloGraphqlServer.handler.ts
+++ packages/infrastructure/src/constructs/api/ApolloGraphqlServer/ApolloGraphqlServer.handler.ts
@@ -1,13 +1,13 @@
 import { PythonRestApi } from './datasources';
 import { Cache } from './cache';
 import { ApolloServer } from 'apollo-server-lambda';
-import compression from 'compression';
-import { schema } from '@cori-risi/graphql-schemas/dist';
+import { schema } from '@cori-risi/graphql-schemas';
 import * as plugins from './plugins';
 
 // Using "import * as express from 'express';" results in "express is not a function" once deployed
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express');
+const compression = require('compression');
 
 const cache = new Cache();
 
