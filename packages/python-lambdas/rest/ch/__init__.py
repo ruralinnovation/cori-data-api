0c07ee10e317ecbb89365816c64353bc9dbe2371
John Hall <john.hall@ruralinnovation.us>
2024-01-19 16:01:25 -0500

    add ch rest endpoint for overall neighbor geos

--- packages/python-lambdas/rest/ch/__init__.py
+++ packages/python-lambdas/rest/ch/__init__.py
@@ -19,6 +19,43 @@ tracer = Tracer(service="BCAT")
 global_params = CONFIG['global']['params']
 
 
+def get_ch_geojson(tab, geoid, attr_table, geom_table, query_fields, simplify, webmercator_srid, where, order_by, limit, offset):
+    # build the query statement
+    query = f"""
+        SELECT
+            json_build_object(
+                'type',       'Feature',
+                'properties', to_jsonb(t.*) - 'geom',
+                'geometry',   ST_AsGeoJSON(geom)::jsonb
+            )
+            FROM (
+                SELECT {query_fields}, st_simplify(st_transform(geom, {webmercator_srid}), {simplify}) as geom
+                    FROM {attr_table} {tab}
+                        INNER JOIN (
+                            SELECT {geoid}, namelsad, geom
+                                FROM {geom_table}
+                        ) geo1 ON {tab}.{geoid} = geo1.{geoid}
+                        {where}
+                    ORDER BY {order_by}
+                    LIMIT {limit}
+                    OFFSET {offset}
+                ) t
+
+        """
+
+    print(query)
+
+    # execute the query string.
+    features = execute(query)
+
+    result = {
+        "type": "FeatureCollection",
+        "features": [f[0] for f in features]
+    }
+
+    return result
+
+
 def get_bcat_count(table):
     print("requesting bcat table count endpoint /<table>/count")
 
@@ -126,7 +163,8 @@ def get_bcat_count(table):
         columns += ", ((ctid::text::point)[0]::bigint<<32 | (ctid::text::point)[1]::bigint) as x_id"
 
     if geom:
-        columns = columns.replace(f'{geom},', f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
+        columns = columns.replace(f'{geom},',
+                                  f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
     else:
         columns += ", ST_GeomFromText('POLYGON EMPTY') as geom"
 
@@ -177,7 +215,6 @@ def get_bcat_count(table):
 
 
 def get_bcat_props(table):
-
     print("requesting bcat table count endpoint /<table>")
 
     print(request.args)
@@ -264,7 +301,6 @@ def get_bcat_props(table):
 
         # handle a potential spatial intersection then remove this parameter and construct the rest.
         if 'geom' in query_params:
-
             criteria += [f"""
                 st_intersects({geom}, st_transform(st_geomfromtext('{query_params['geom']}', {webmercator_srid}), {epsg}))
                 """]
@@ -377,7 +413,6 @@ def get_bcat_props(table):
 
 
 def get_bcat_geojson(table):
-
     print("requesting bcat table count endpoint /<table>")
 
     print(request.args)
@@ -414,7 +449,8 @@ def get_bcat_geojson(table):
         columns += ", ((ctid::text::point)[0]::bigint<<32 | (ctid::text::point)[1]::bigint) as x_id"
 
     if geom:
-        columns = columns.replace(f'{geom},', f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
+        columns = columns.replace(f'{geom},',
+                                  f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
     else:
         columns += ", ST_GeomFromText('POLYGON EMPTY') as geom"
 
@@ -466,7 +502,6 @@ def get_bcat_geojson(table):
 
         # handle a potential spatial intersection then remove this parameter and construct the rest.
         if 'geom' in query_params:
-
             criteria += [f"""
                 st_intersects({geom}, st_transform(st_geomfromtext('{query_params['geom']}', {webmercator_srid}), {epsg}))
                 """]
@@ -534,16 +569,194 @@ def get_bcat_geojson(table):
     )
 
 
+def get_ch_overall_neighbor_geo(tab):
+    table = f'ch_app_wide_{tab}'
 
-def get_bbox_at_location(tab):
-
-    print("requesting bcat table count endpoint /<table>")
+    print(f'requesting ch neighboring geos for /{table}')
 
     print(request.args)
 
     # print(types.BuiltinFunctionType)
 
+    if table not in CONFIG:
+        raise BadRequestError(f'invalid table {table}')
+
+    webmercator_srid = 4326
+    attr_table = CONFIG[table]['table']
+    db_table = CONFIG[table].get('table', table)
+    geom_table = CONFIG[f'{table}_geo']['table']
+    columns = CONFIG[table].get('api_columns', '*')
+    geom = CONFIG[table].get('geom', None)
+    epsg = CONFIG[table].get('epsg', None)
+    limit = ''  # Option to limit the total number of records returned. Don't include this key in the config to disable
+    if 'limit' in CONFIG[table]:
+        limit = CONFIG[table].get('limit', LIMIT)
+    else:
+        limit = LIMIT
+    offset = OFFSET
+    page = PAGE
+    id = CONFIG[table].get('id', None)
+    id_in_result = ""
+    geoid = CONFIG[table].get('geoid', None)
+    order_by = f'{geoid}'
+    params = CONFIG[table]['params']
+    simplify = CONFIG[table].get('simplify', 0.0001)
+
+    if id:
+        columns = columns.replace(f'{id},', f'"{id}" as x_id, {id},')
+        id_in_result = "'id',         x_id,"
+    else:
+        # if no id then use somewhat hacky ctid to bigint method.
+        # WARNING: only works if there are no changes to table rows!!
+        columns += ", ((ctid::text::point)[0]::bigint<<32 | (ctid::text::point)[1]::bigint) as x_id"
+
+    if geom:
+        columns = columns.replace(f'{geom},',
+                                  f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
+    else:
+        columns += ", ST_GeomFromText('POLYGON EMPTY') as geom"
+
+    print(columns)
+
+    # criteria is a list of where clauses for the query.
+    criteria = []
+
+    if type(request.args.keys) is types.BuiltinFunctionType and len(request.args.keys()) > 0:
+        print("URL query params is not empty")
+        invalid_params = [k for k in request.args.keys() if k not in (global_params + params)]
+        if invalid_params:
+            raise BadRequestError(f'invalid parameter {invalid_params}')
+
+        query_params = {k: [v, ] for k, v in request.args.items()}
+
+        logger.info(query_params)
+        print(query_params)
+
+        # Get list of available vars for this geoid
+        if f'{geoid}' not in query_params.keys():
+            raise BadRequestError(f'missing {geoid}')
+        else:
+            variables = [ "overall_loss_score", "overall_loss_rating", "pct_bb_25_3", "pct_bb_100_20", "pct_bb_fiber" ]
+            for var in variables:
+                print(var)
+
+        print(f'geoid is {geoid}: {query_params[geoid]}')
+
+        # Add namelsad to list of variables
+        variables.insert(0, 'namelsad as name')
+
+        # Add geoid label ('geoid_co' | 'geoid_tr') to list of variables
+        variables.insert(0, f'{tab}.{geoid}')
+
+        query_fields = ", ".join(variables)
+
+        print(f'with query_fields: {query_fields}')
+
+        if ';' in str(query_params):
+            raise BadRequestError(f'invalid parameter')
+
+        for k, v in query_params.items():
+            print(f'{k} = {v}')
+
+            if 'limit' in query_params and k == 'limit':
+                limit = int(v[0])
+                # del query_params['limit']
+
+            if 'offset' in query_params and k == 'offset':
+                offset = int(v[0])
+                # del query_params['offset']
+
+            if 'page' in query_params and k == 'page':
+                page = int(v[0])
+                # del query_params['page']
+
+                if page > 0:
+                    offset = page * limit
+
+        if 'limit' in query_params:
+            del query_params['limit']
+
+        if 'offset' in query_params:
+            del query_params['offset']
+
+        if 'page' in query_params:
+            del query_params['page']
+
+        # handle a potential spatial intersection then remove this parameter and construct the rest.
+        if 'geom' in query_params:
+            criteria += [f"""
+                st_intersects({geom}, st_transform(st_geomfromtext('{query_params['geom']}', {webmercator_srid}), {epsg}))
+                """]
+
+            del query_params['geom']
+
+        # since we want to handle one or more parameter values coerce all to list
+        # construct "any" style array literal predicates like: where geoid = any('{123, 456}')
+        # query_params.update({k: [v, ] for k, v in query_params.items() if type(v) != list})
+        # query_params.update({k: "ANY('{" + ",".join(v) + "}')" for k, v in query_params.items()})
+        conditional_values = {k: [v, ] for k, v in query_params.items() if type(v) != list}
+        for k, v in query_params.items():
+            # criteria += [f'{k} = {v}', ]
+            if k == geoid:
+                print(v[0])
+                print(type(v[0]))
+                if geoid == "geoid_co":
+                    fips_st = v[0][0:2] # get state fips from first two chars of geoid
+                    print(fips_st)
+                    where = f'WHERE {geoid} ilike ' + "'" + fips_st + "%'"
+                elif geoid == "geoid_tr":
+                    geoid_co = v[0][0:5] # get county geoid from first five chars of geoid
+                    print(geoid_co)
+                    where = f'WHERE {geoid} ilike ' + "'" + geoid_co + "%'"
+
+    else:
+        print("URL query params is empty")
+
+    print('criteria:')
+    print(criteria)
+
+    # join the criteria so that we get the right syntax for any number of clauses
+    where = ''
+    if criteria:
+        where = 'WHERE ' + ' AND '.join(criteria)
+
+    # build the first query statement
+    query = f"""
+        SELECT {geoid}
+            FROM {attr_table}
+            {where}
+            ORDER BY {order_by}
+        """
+
+    print(query)
+
+    # execute the query string.
+    geoids = "ANY('{" + ",".join([item for sublist in execute(query) for item in sublist]) + "}')"
+    where = f'WHERE {tab}.{geoid} = {geoids}'
+
+    result = get_ch_geojson(tab, geoid, attr_table, geom_table, query_fields, simplify, webmercator_srid, where, order_by, limit, offset)
+
+    return Response(
+        response=json.dumps(result, indent=None),
+        status=200,
+        content_type='application/json',
+        headers={
+            'access-control-allow-origin': '*',
+            'access-control-allow-headers': 'Authorization,Content-Type,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent',
+            'access-control-allow-methods': 'OPTIONS,GET,PUT,POST,DELETE,PATCH,HEAD',
+            'access-control-allow-credentials': 'true'
+        }
+    )
+
+
+def get_bbox_at_location(tab):
     table = f'ch_app_wide_{tab}_geo'
+
+    print(f'requesting ch geo bbox at location for /{table}')
+
+    print(request.args)
+
+    # print(types.BuiltinFunctionType)
 
     if table not in CONFIG:
         raise BadRequestError(f'invalid table {table}')
@@ -576,7 +789,8 @@ def get_bbox_at_location(tab):
         columns += ", ((ctid::text::point)[0]::bigint<<32 | (ctid::text::point)[1]::bigint) as x_id"
 
     if geom:
-        columns = columns.replace(f'{geom},', f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
+        columns = columns.replace(f'{geom},',
+                                  f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
     else:
         columns += ", ST_GeomFromText('POLYGON EMPTY') as geom"
 
@@ -648,7 +862,6 @@ def get_bbox_at_location(tab):
 
         # handle a potential spatial intersection then remove this parameter and construct the rest.
         if 'geom' in query_params:
-
             criteria += [f"""
                 st_intersects({geom}, st_transform(st_geomfromtext('{query_params['geom']}', {webmercator_srid}), {epsg}))
                 """]
@@ -737,16 +950,14 @@ def get_bbox_at_location(tab):
     )
 
 
-
 def get_bb_map(tab):
+    table = f'bb_map_{tab}'
 
-    print("requesting bcat table count endpoint /<table>")
+    print(f'requesting ch bb map for /{table}')
 
     print(request.args)
 
     # print(types.BuiltinFunctionType)
-
-    table = f'bb_map_{tab}'
 
     if table not in CONFIG:
         raise BadRequestError(f'invalid table {table}')
@@ -778,7 +989,8 @@ def get_bb_map(tab):
         columns += ", ((ctid::text::point)[0]::bigint<<32 | (ctid::text::point)[1]::bigint) as x_id"
 
     if geom:
-        columns = columns.replace(f'{geom},', f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
+        columns = columns.replace(f'{geom},',
+                                  f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
     else:
         columns += ", ST_GeomFromText('POLYGON EMPTY') as geom"
 
@@ -830,7 +1042,6 @@ def get_bb_map(tab):
 
         # handle a potential spatial intersection then remove this parameter and construct the rest.
         if 'geom' in query_params:
-
             criteria += [f"""
                 st_intersects({geom}, st_transform(st_geomfromtext('{query_params['geom']}', {webmercator_srid}), {epsg}))
                 """]
@@ -896,4 +1107,3 @@ def get_bb_map(tab):
             'access-control-allow-credentials': 'true'
         }
     )
-
