e12faee97e6c21ec3cab2b35efc146efaa5e43e9
John Hall <john.hall@ruralinnovation.us>
2024-02-14 15:22:35 -0500

    add isp combo endpoints

--- packages/python-lambdas/rest/bead/__init__.py
+++ packages/python-lambdas/rest/bead/__init__.py
@@ -17,6 +17,158 @@ tracer = Tracer(service="BEAD")
 global_params = CONFIG['global']['params']
 
 
+def get_bead_isp_id_to_combo():
+    table = "isp_id_to_combo_isp_id"
+
+    print(f'requesting bead isp tech from {table}')
+
+    print(request.args)
+
+    # print(types.BuiltinFunctionType)
+
+    if table not in CONFIG:
+        raise BadRequestError(f'invalid table {table}')
+
+    db_table = CONFIG[table].get('table', table)
+    columns = CONFIG[table].get('api_columns', '*')
+    limit = ''  # Option to limit the total number of records returned. Don't include this key in the config to disable
+    if 'limit' in CONFIG[table]:
+        limit = CONFIG[table].get('limit', LIMIT)
+    else:
+        limit = LIMIT
+    offset = OFFSET
+    order_by = "isp_id::int, new_alias"
+    params = CONFIG[table]['params']
+
+    columns += ", 'isp_combo' as type"
+
+    print(columns)
+
+    # criteria is a list of where clauses for the query.
+    criteria = []
+
+    if type(request.args.keys) is types.BuiltinFunctionType and len(request.args.keys()) > 0:
+        print("URL query params is not empty")
+        invalid_params = [k for k in request.args.keys() if k not in (global_params + params)]
+        if invalid_params:
+            raise BadRequestError(f'invalid parameter {invalid_params}')
+
+        query_params = {k: [v, ] for k, v in request.args.items()}
+
+        logger.info(query_params)
+        print(query_params)
+
+        # Get list of available vars for this geoid
+        if 'isp_id' not in query_params.keys():
+            raise BadRequestError(f'missing isp_id')
+        elif ';' in str(query_params):
+            raise BadRequestError(f'invalid parameter')
+
+        print(f'geoid is isp_id: {query_params["isp_id"]}')
+
+        for k, v in query_params.items():
+            print(f'{k} = {v}')
+
+            if 'limit' in query_params and k == 'limit':
+                limit = int(v[0])
+                # del query_params['limit']
+
+            if 'offset' in query_params and k == 'offset':
+                offset = int(v[0])
+                # del query_params['offset']
+
+            if 'page' in query_params and k == 'page':
+                page = int(v[0])
+                # del query_params['page']
+
+                if page > 0:
+                    offset = page * limit
+
+        if 'limit' in query_params:
+            del query_params['limit']
+
+        if 'offset' in query_params:
+            del query_params['offset']
+
+        if 'page' in query_params:
+            del query_params['page']
+
+        # since we want to handle one or more parameter values coerce all to list
+        # construct "any" style array literal predicates like: where geoid = any('{123, 456}')
+        query_params.update({k: [v, ] for k, v in query_params.items() if type(v) != list})
+        query_params.update({k: "ANY('{" + ",".join(v) + "}')" for k, v in query_params.items()})
+        for k, v in query_params.items():
+            criteria += [f'{k} = {v}', ]
+
+    else:
+        print("URL query params is empty")
+
+    print('criteria:')
+    print(criteria)
+
+    # build the query statement
+    if criteria:
+        # join the criteria so that we get the right syntax for any number of clauses
+        where = 'WHERE ' + ' AND '.join(criteria)
+        query = f"""
+            SELECT
+                json_build_object(
+                    'type',       'Feature',
+                    'properties', to_jsonb(t.*)
+                )
+                FROM (
+                    SELECT {columns}
+                        FROM {db_table}
+                        {where}
+                        GROUP BY isp_id, new_alias
+                        ORDER BY {order_by}
+                        LIMIT {limit}
+                        OFFSET {offset}
+                    ) t
+        """
+    else:
+        query = f"""
+            SELECT
+                json_build_object(
+                    'type',       'Feature',
+                    'properties', to_jsonb(t.*)
+                )
+                FROM (
+                    SELECT DISTINCT isp_id::int, new_alias, array_to_string(ARRAY_AGG(combo_isp_id),',') as combo_isp_id, 'isp_combo' as type
+                        FROM {db_table}
+                        GROUP BY isp_id, new_alias
+                        ORDER BY {order_by}
+                        LIMIT {limit}
+                        OFFSET {offset}
+                    ) t
+        """
+
+    print(query)
+
+    # execute the query string.
+    features = execute(query)
+
+    # print(features)
+
+    result = {
+        "type": "FeatureCollection",
+        "count": len(features),
+        "features": [f[0] for f in features]
+    }
+
+    return Response(
+        response=json.dumps(result, indent=None),
+        status=200,
+        content_type='application/json',
+        headers={
+            'access-control-allow-origin': '*',
+            'access-control-allow-headers': 'Authorization,Content-Type,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent',
+            'access-control-allow-methods': 'OPTIONS,GET,PUT,POST,DELETE,PATCH,HEAD',
+            'access-control-allow-credentials': 'true'
+        }
+    )
+
+
 def get_bead_geojson():
     table = "bead_bl"
 
@@ -196,7 +348,7 @@ def get_bead_isp_tech():
     else:
         limit = LIMIT
     offset = OFFSET
-    order_by = f'new_alias, isp_id'
+    order_by = f'new_alias, isp_id::int'
     params = CONFIG[table]['params']
 
     columns += ", 'isp_tech' as type"
@@ -470,11 +622,12 @@ def get_bead_detailed_info(tab):
 
     match tab:
         case "geojson":
-            # Try to get just tech
             features = get_bead_geojson()
 
+        case "isp_combo":
+            features = get_bead_isp_tech()
+
         case "isp_tech":
-            # Try to get just tech
             features = get_bead_isp_tech()
 
         case "rdof":
