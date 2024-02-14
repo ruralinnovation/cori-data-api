1b907e1a83190a477de23594666b4c54baa5a023
John Hall <john.hall@ruralinnovation.us>
2024-02-14 15:22:35 -0500

    add isp combo endpoints

--- packages/python-lambdas/rest/bead/bead_config.py
+++ packages/python-lambdas/rest/bead/bead_config.py
@@ -17,7 +17,7 @@ bead configuration
 
 CONFIG = {
     "global": {
-        "params": [ "limit", "offset", "page" ],
+        "params": ["limit", "offset", "page"],
         "epsg": 4269,
         "simplify": 0.0001,
         "precision": 6
@@ -31,22 +31,32 @@ CONFIG = {
     #     "epsg": None,
     #     "id": None
     # },
+    "isp_id_to_combo_isp_id": {
+        "table": "proj_bead.isp_id_to_combo_isp_id_v1",
+        "api_columns": "isp_id::int, new_alias, array_to_string(ARRAY_AGG(agg_isp_id),',') as agg_isp_id, array_to_string(ARRAY_AGG(combo_isp_id),',') as combo_isp_id",
+        "params": ["isp_id"],
+        "geoid": None,
+        "geom": None,
+        "epsg": None,
+        "id": None,
+        "limit": 100000  # some tracts have a *lot* of blocks
+    },
     "bead_bl": {
         "table": "proj_bead.bead_blocksv1",
         "api_columns": "geoid_bl, geoid_tr, geoid_co, geoid_st, bead_category, bl_25_3_area, bl_100_20_area, cnt_25_3, cnt_100_20, cnt_total_locations, cnt_isp, combo_isp_id, isp_id, pct_served, has_previous_funding, has_copperwire, has_coaxial_cable, has_fiber, has_licensed_wireless, has_lbr_wireless, only_water_flag, geom",
-        "params": [ "geoid_bl" ],
+        "params": ["geoid_bl"],
         "geoid": "geoid_bl",
         "geom": "geom",
         "epsg": 4269,
         "id": "geoid_bl",
         "simplify": 0.0,
         "precision": 6,
-        "limit": 1000 # some tracts have a *lot* of blocks
+        "limit": 1000  # some tracts have a *lot* of blocks
     },
     "isp_tech_bl": {
         "table": "proj_bead.isp_tech_bl",
         "api_columns": "geoid_bl, new_alias, isp_id, technology, max_down, max_up",
-        "params": [ "geoid_bl", "isp_id", "technology" ],
+        "params": ["geoid_bl", "isp_id", "technology"],
         "geoid": "geoid_bl",
         "geom": None,
         "epsg": None,
@@ -56,7 +66,7 @@ CONFIG = {
         "table": "proj_bead.rdof_bl",
         "alias": "rdof",
         "api_columns": "rdof.applicant, rdof.winning_bi, rdof.state, county, rdof.geoid_bl, rdof.da_numbers, rdof.geoid_co, rdof.tier, rdof.latency, rdof.frn, rdof.sac, rdof.winning_bidder, rdof.winning_bid_total_in_state, rdof.number_of_locations_in_state, rdof.authorized, rdof.\"default\", rdof.version",
-        "params": [ "applicant", "geoid_bl", "geoid_co", "version" ],
+        "params": ["applicant", "geoid_bl", "geoid_co", "version"],
         "geoid": "geoid_bl",
         "geom": None,
         "epsg": None,
