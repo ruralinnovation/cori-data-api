5acd3317b972cb06131fabf0dc94dad52ac43c40
John Hall <john.hall@ruralinnovation.us>
2024-03-05 21:36:40 -0500

    improvements for bead

--- packages/python-lambdas/rest/bead/__init__.py
+++ packages/python-lambdas/rest/bead/__init__.py
@@ -317,6 +317,159 @@ def get_bead_tract_acs():
     return features
 
 
+# def get_bead_county_info():
+#     table = "acs_wide_tr"
+#
+#     print(f'requesting bead tract acs from {table}')
+#
+#     print(request.args)
+#
+#     # print(types.BuiltinFunctionType)
+#
+#     if table not in CONFIG:
+#         raise BadRequestError(f'invalid table {table}')
+#
+#     webmercator_srid = 4326
+#     block_table = CONFIG["bead_bl"].get('table', "proj_bead.bead_blockv1b")
+#     block_columns = "string_agg(geoid_bl, ',') as geoid_bl, geoid_tr"
+#     db_table = CONFIG[table].get('table', table)
+#     columns = CONFIG[table].get('api_columns', '*')
+#     id = CONFIG[table].get('id', None)
+#     id_in_result = ""
+#     # geoid = CONFIG[table].get('geoid', None)
+#     # geom = CONFIG[table].get('geom', None)
+#     # epsg = CONFIG[table].get('epsg', None)
+#     limit = ''  # Option to limit the total number of records returned. Don't include this key in the config to disable
+#     if 'limit' in CONFIG[table]:
+#         limit = CONFIG[table].get('limit', LIMIT)
+#     else:
+#         limit = LIMIT
+#     offset = OFFSET
+#     page = PAGE
+#     params = CONFIG[table]['params']
+#     order_by = ', '.join([x for x in params if x != 'geom'])
+#     simplify = CONFIG[table].get('simplify', 0.0)
+#
+#     columns += ", 'acs' as type"
+#
+#     if id:
+#         columns = columns.replace(f'{id},', f'{id}, "{id}" as x_id,')
+#         id_in_result = "'id',         x_id,"
+#     else:
+#         # if no id then use somewhat hacky ctid to bigint method.
+#         # WARNING: only works if there are no changes to table rows!!
+#         columns += ", ((ctid::text::point)[0]::bigint<<32 | (ctid::text::point)[1]::bigint) as x_id"
+#
+#     # if geom:
+#     #     columns = columns.replace(f'{geom},',
+#     #                               f'st_simplify(st_transform({geom}, {webmercator_srid}), {simplify}) as geom, ')
+#     # else:
+#     #     columns += ", ST_GeomFromText('POLYGON EMPTY') as geom"
+#
+#     print(columns)
+#
+#     # criteria is a list of where clauses for the query.
+#     criteria = []
+#
+#     if type(request.args.keys) is types.BuiltinFunctionType and len(request.args.keys()) > 0:
+#         print("URL query params is not empty")
+#         invalid_params = [k for k in request.args.keys() if k not in (global_params + params)]
+#         if invalid_params:
+#             raise BadRequestError(f'invalid parameter {invalid_params}')
+#
+#         query_params = {k: [v, ] for k, v in request.args.items()}
+#
+#         logger.info(query_params)
+#         print(query_params)
+#
+#         if ';' in str(query_params):
+#             raise BadRequestError(f'invalid parameter')
+#
+#         for k, v in query_params.items():
+#             print(f'{k} = {v}')
+#
+#             if 'limit' in query_params and k == 'limit':
+#                 limit = int(v[0])
+#                 # del query_params['limit']
+#
+#             if 'offset' in query_params and k == 'offset':
+#                 offset = int(v[0])
+#                 # del query_params['offset']
+#
+#             if 'page' in query_params and k == 'page':
+#                 page = int(v[0])
+#                 # del query_params['page']
+#
+#                 if page > 0:
+#                     offset = page * limit
+#
+#         if 'limit' in query_params:
+#             del query_params['limit']
+#
+#         if 'offset' in query_params:
+#             del query_params['offset']
+#
+#         if 'page' in query_params:
+#             del query_params['page']
+#
+#         # # handle a potential spatial intersection then remove this parameter and construct the rest.
+#         # if 'geom' in query_params:
+#         #     criteria += [f"""
+#         #         st_intersects({geom}, st_transform(st_geomfromtext('{query_params['geom']}', {webmercator_srid}), {epsg}))
+#         #         """]
+#         #
+#         #     del query_params['geom']
+#
+#         # since we want to handle one or more parameter values coerce all to list
+#         # construct "any" style array literal predicates like: where geoid = any('{123, 456}')
+#         query_params.update({k: [v, ] for k, v in query_params.items() if type(v) != list})
+#         query_params.update({k: "ANY('{" + ",".join(v) + "}')" for k, v in query_params.items()})
+#         for k, v in query_params.items():
+#             criteria += [f'{k} = {v}', ]
+#
+#     else:
+#         print("URL query params is empty")
+#
+#     print('criteria:')
+#     print(criteria)
+#
+#     # join the criteria so that we get the right syntax for any number of clauses
+#     where = ''
+#     if criteria:
+#         where = 'WHERE ' + ' AND '.join(criteria)
+#
+#     # build the query statement
+#     query = f"""
+#         SELECT
+#             json_build_object(
+#                 {id_in_result}
+#                 'type',       'Feature',
+#                 'properties', to_jsonb(t.*) - 'x_id'
+#             )
+#             FROM (
+#                 SELECT {columns}
+#                     FROM {db_table} tr, (
+#                         SELECT {block_columns}
+#                             FROM {block_table}
+#                             {where}
+#                             GROUP BY geoid_tr
+#                     ) bl
+#                     WHERE tr.geoid = bl.geoid_tr
+#                     ORDER BY {order_by}
+#                     LIMIT {limit}
+#                     OFFSET {offset}
+#                 ) t
+#
+#         """
+#
+#     print(query)
+#
+#     # execute the query string.
+#     features = execute(query)
+#
+#     return features
+
+
 def get_bead_isp_id_to_combo():
     table = "isp_id_to_combo_isp_id"
 
@@ -608,12 +761,12 @@ def get_bead_isp_tech():
                         OFFSET {offset}
                     ) t
         """
-        
+
     print(query)
-    
+
     # execute the query string.
     features = execute(query)
-    
+
     return features
 
 
@@ -742,7 +895,7 @@ def get_bead_award_rdof():
                 'properties', to_jsonb(t.*) - 'x_id'
             )
             FROM (
-                SELECT {columns}
+                SELECT award_bl.geoid_bl as geoid_bl, {columns}
                     FROM (
                         SELECT DISTINCT geoid_bl, award_geoid_bl
                             FROM proj_bead.award_bl
