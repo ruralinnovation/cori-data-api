03f1119b57a0a88280caea3ce914971d76f541d3
John Hall <john.hall@ruralinnovation.us>
2024-04-10 08:04:54 -0400

    serve bfm awards data

--- packages/python-lambdas/rest/bead/__init__.py
+++ packages/python-lambdas/rest/bead/__init__.py
@@ -618,24 +618,43 @@ def get_bead_isp_tech():
 
 
 """
-Query:
-SELECT award_bl.award_geoid_bl, rdof.*, 'award' as type
-	FROM (
-		SELECT geoid_bl, award_geoid_bl
-			FROM proj_bead.award_bl
-			WHERE geoid_bl = ANY('{010539698023012}') --  == 010539698003009
-	) award_bl, proj_bead.rdof_bl rdof
-	WHERE award_bl.award_geoid_bl = rdof.geoid_bl
-	ORDER BY geoid_bl
-	LIMIT 500
-	OFFSET 0;
+Queries:
+
+WITH bfm_award_counties as (
+			SELECT geoid_st, geoid_co, geoid_bl, project_id, program_id
+				FROM proj_bead."bfm_award_bl_V3"
+				WHERE geoid_bl = ANY('{
+					120330039003044,
+					120330039003036,
+					120330039003051,
+					120330039003048,
+					120330039003052,
+					120330039003036,
+					121199106022001,
+					121199106022000,
+					011339659002026,
+					010059502002015,
+					011339659002034,
+					010950305024025,
+					010950305021039,
+					011030056012058,
+					011339659002065
+				}')
+)
+SELECT DISTINCT awards_co.brandname, awards_co.providerid, awards_co.program_id, awards_co.geoid_co, awards_co.geoid_st, awards_co.build_req, awards_co.loc_plan, awards_co.loc_sup, awards_co.technology, awards_co.maxdown, awards_co.maxup, awards_co.project, awards_co.tranche, awards_co.fund_awarded, awards_co.fund_expended, awards_co.fund_loan, awards_co.fund_grant, awards_co.fund_ob, awards_co.project_cost, awards_co.proj_start, awards_co.proj_end, awards_co.tribal_id, awards_co.tribal_location_pct, awards_co.tribal_funding, awards_co.cadence
+	FROM proj_bead."bfm_award_co_V3" awards_co 
+	LEFT JOIN bfm_award_counties
+	ON awards_co.project_id = bfm_award_counties.project_id
+	WHERE awards_co.geoid_co = bfm_award_counties.geoid_co
+	LIMIT 500;
+
 """
 
 
-def get_bead_award_rdof():
-    table = "rdof_bl"
+def get_bead_previous_awards():
+    table = "bfm_award_co"
 
-    print(f'requesting bead previous rdof award status from {table}')
+    print(f'requesting bead previous federal awards from {table}')
 
     print(request.args)
 
@@ -646,6 +665,9 @@ def get_bead_award_rdof():
 
     db_table = CONFIG[table].get('table', table)
     db_alias = CONFIG[table].get('alias', table)
+    db_lookup_table = CONFIG["bfm_award_bl"].get('table', table)
+    db_lookup_alias = CONFIG["bfm_award_bl"].get('alias', table)
+    db_lookup_columns = CONFIG["bfm_award_bl"].get('api_columns', table)
     columns = CONFIG[table].get('api_columns', '*')
     geoid = CONFIG[table].get('geoid', None)
     limit = ''  # Option to limit the total number of records returned. Don't include this key in the config to disable
@@ -661,10 +683,6 @@ def get_bead_award_rdof():
 
     columns += ", 'award' as type"
 
-    # if no id then use somewhat hacky ctid to bigint method.
-    # WARNING: only works if there are no changes to table rows!!
-    columns += ", ((ctid::text::point)[0]::bigint<<32 | (ctid::text::point)[1]::bigint) as x_id"
-
     print(columns)
 
     # criteria is a list of where clauses for the query.
@@ -674,6 +692,7 @@ def get_bead_award_rdof():
         print("URL query params is not empty")
         invalid_params = [k for k in request.args.keys() if k not in (global_params + params)]
         if invalid_params:
+            print(f'invalid parameter {invalid_params}')
             raise BadRequestError(f'invalid parameter {invalid_params}')
 
         query_params = {k: [v, ] for k, v in request.args.items()}
@@ -681,13 +700,13 @@ def get_bead_award_rdof():
         logger.info(query_params)
         print(query_params)
 
-        # Get list of available vars for this geoid
-        if f'{geoid}' not in query_params.keys():
-            raise BadRequestError(f'missing {geoid}')
-        elif ';' in str(query_params):
-            raise BadRequestError(f'invalid parameter')
-
-        print(f'geoid is {geoid}: {query_params[geoid]}')
+        # # Get list of available vars for this geoid
+        # if f'{geoid}' not in query_params.keys():
+        #     raise BadRequestError(f'missing {geoid}')
+        # elif ';' in str(query_params):
+        #     raise BadRequestError(f'invalid parameter')
+        #
+        # print(f'geoid is {geoid}: {query_params[geoid]}')
 
         for k, v in query_params.items():
             print(f'{k} = {v}')
@@ -739,20 +758,22 @@ def get_bead_award_rdof():
         SELECT
             json_build_object(
                 'type',       'Feature',
-                'properties', to_jsonb(t.*) - 'x_id'
+                'properties', to_jsonb(t.*)
             )
             FROM (
-                SELECT award_bl.geoid_bl as geoid_bl, {columns}
-                    FROM (
-                        SELECT DISTINCT geoid_bl, award_geoid_bl
-                            FROM proj_bead.award_bl
-                            {where}
-                    ) award_bl, {db_table} {db_alias}
-                    WHERE award_bl.award_geoid_bl = rdof.geoid_bl
-                    ORDER BY {order_by}
+                WITH {db_lookup_alias} as (
+                    SELECT {db_lookup_columns}
+                        FROM {db_lookup_table}
+                        {where}
+                )
+                SELECT DISTINCT {columns}
+                    FROM {db_table} {db_alias}
+                    LEFT JOIN {db_lookup_alias}
+                    ON {db_alias}.project_id = {db_lookup_alias}.project_id
+                    WHERE {db_alias}.geoid_co = {db_lookup_alias}.geoid_co
                     LIMIT {limit}
                     OFFSET {offset}
-                ) t
+            ) t
 
         """
 
@@ -762,6 +783,153 @@ def get_bead_award_rdof():
     features = execute(query)
 
     return features
+
+
+# """
+# Query:
+# SELECT award_bl.award_geoid_bl, rdof.*, 'award' as type
+# 	FROM (
+# 		SELECT geoid_bl, award_geoid_bl
+# 			FROM proj_bead.award_bl
+# 			WHERE geoid_bl = ANY('{010539698023012}') --  == 010539698003009
+# 	) award_bl, proj_bead.rdof_bl rdof
+# 	WHERE award_bl.award_geoid_bl = rdof.geoid_bl
+# 	ORDER BY geoid_bl
+# 	LIMIT 500
+# 	OFFSET 0;
+# """
+#
+#
+# def get_bead_award_rdof():
+#     table = "rdof_bl"
+#
+#     print(f'requesting bead previous rdof award status from {table}')
+#
+#     print(request.args)
+#
+#     # print(types.BuiltinFunctionType)
+#
+#     if table not in CONFIG:
+#         raise BadRequestError(f'invalid table {table}')
+#
+#     db_table = CONFIG[table].get('table', table)
+#     db_alias = CONFIG[table].get('alias', table)
+#     columns = CONFIG[table].get('api_columns', '*')
+#     geoid = CONFIG[table].get('geoid', None)
+#     limit = ''  # Option to limit the total number of records returned. Don't include this key in the config to disable
+#     if 'limit' in CONFIG[table]:
+#         limit = CONFIG[table].get('limit', LIMIT)
+#     else:
+#         limit = LIMIT
+#     offset = OFFSET
+#     page = PAGE
+#     params = CONFIG[table]['params']
+#     order_by = f'{db_alias}.{geoid}'
+#     params = CONFIG[table]['params']
+#
+#     columns += ", 'award' as type"
+#
+#     # if no id then use somewhat hacky ctid to bigint method.
+#     # WARNING: only works if there are no changes to table rows!!
+#     columns += ", ((ctid::text::point)[0]::bigint<<32 | (ctid::text::point)[1]::bigint) as x_id"
+#
+#     print(columns)
+#
+#     # criteria is a list of where clauses for the query.
+#     criteria = []
+#
+#     if type(request.args.keys) is types.BuiltinFunctionType and len(request.args.keys()) > 0:
+#         print("URL query params is not empty")
+#         invalid_params = [k for k in request.args.keys() if k not in (global_params + params)]
+#         if invalid_params:
+#             raise BadRequestError(f'invalid parameter {invalid_params}')
+#
+#         query_params = {k: [v, ] for k, v in request.args.items()}
+#
+#         logger.info(query_params)
+#         print(query_params)
+#
+#         # Get list of available vars for this geoid
+#         if f'{geoid}' not in query_params.keys():
+#             raise BadRequestError(f'missing {geoid}')
+#         elif ';' in str(query_params):
+#             raise BadRequestError(f'invalid parameter')
+#
+#         print(f'geoid is {geoid}: {query_params[geoid]}')
+#
+#         for k, v in query_params.items():
+#             print(f'{k} = {v}')
+#
+#             if 'limit' in query_params and k == 'limit':
+#                 limit = int(v[0])
+#                 # del query_params['limit']
+#
+#             if 'offset' in query_params and k == 'offset':
+#                 offset = int(v[0])
+#                 # del query_params['offset']
+#
+#             if 'page' in query_params and k == 'page':
+#                 page = int(v[0])
+#                 # del query_params['page']
+#
+#                 if page > 0:
+#                     offset = page * limit
+#
+#         if 'limit' in query_params:
+#             del query_params['limit']
+#
+#         if 'offset' in query_params:
+#             del query_params['offset']
+#
+#         if 'page' in query_params:
+#             del query_params['page']
+#
+#         # since we want to handle one or more parameter values coerce all to list
+#         # construct "any" style array literal predicates like: where geoid = any('{123, 456}')
+#         query_params.update({k: [v, ] for k, v in query_params.items() if type(v) != list})
+#         query_params.update({k: "ANY('{" + ",".join(v) + "}')" for k, v in query_params.items()})
+#         for k, v in query_params.items():
+#             criteria += [f'{k} = {v}', ]
+#
+#     else:
+#         print("URL query params is empty")
+#
+#     print('criteria:')
+#     print(criteria)
+#
+#     # join the criteria so that we get the right syntax for any number of clauses
+#     where = ''
+#     if criteria:
+#         where = 'WHERE ' + ' AND '.join(criteria)
+#
+#     # build the query statement
+#     query = f"""
+#         SELECT
+#             json_build_object(
+#                 'type',       'Feature',
+#                 'properties', to_jsonb(t.*) - 'x_id'
+#             )
+#             FROM (
+#                 SELECT award_bl.geoid_bl as geoid_bl, {columns}
+#                     FROM (
+#                         SELECT DISTINCT geoid_bl, award_geoid_bl
+#                             FROM proj_bead.award_bl
+#                             {where}
+#                     ) award_bl, {db_table} {db_alias}
+#                     WHERE award_bl.award_geoid_bl = rdof.geoid_bl
+#                     ORDER BY {order_by}
+#                     LIMIT {limit}
+#                     OFFSET {offset}
+#                 ) t
+#
+#         """
+#
+#     print(query)
+#
+#     # execute the query string.
+#     features = execute(query)
+#
+#     return features
 
 
 def get_bead_detailed_info(tab):
@@ -786,15 +954,19 @@ def get_bead_detailed_info(tab):
         case "isp_tech":
             features = get_bead_isp_tech()
 
-        case "rdof":
-            features = get_bead_award_rdof()
+        case "award":
+            features = get_bead_previous_awards()
+
+        # case "rdof":
+        #     features = get_bead_award_rdof()
 
         case _:
             geojson_features = get_bead_geojson()
             acs = get_bead_tract_acs()
             isp_features = get_bead_isp_tech()
-            rdof_features = get_bead_award_rdof()
-            features = geojson_features + acs + isp_features + rdof_features
+            awards_features = get_bead_previous_awards()
+            # rdof_features = get_bead_award_rdof()
+            features = geojson_features + acs + isp_features + awards_features
 
     # print(features)
 
