ecf398dc0bdb2b2f8e5e168735faa5d296046c46
John Hall <john.hall@ruralinnovation.us>
2024-04-15 14:43:40 -0400

    fix

--- packages/python-lambdas/rest/bead/bead_config.py
+++ packages/python-lambdas/rest/bead/bead_config.py
@@ -24,7 +24,9 @@ CONFIG = {
     },
     "acs_wide_co": {
         "table": "proj_bead.acs_wide_co",
-        "api_columns": "geoid as geoid_co, year, total_population, total_households, total_housing_units, hh_w_computer, hh_w_smartphone_only, hh_wo_device, hh_using_broadband, geoid_bl",
+        "alias": "acs_co",
+        "api_columns": "acs_co.geoid_co, acs_co.year, acs_co.total_population, acs_co.total_households, acs_co.total_housing_units, "
+                       "acs_co.hh_w_computer, acs_co.hh_w_smartphone_only, acs_co.hh_wo_device, acs_co.hh_using_broadband",
         "params": ["geoid_co", "geoid_bl"],
         "geoid": "geoid_co",
         "geom": None,
@@ -33,7 +35,9 @@ CONFIG = {
     },
     # "acs_wide_tr": {
     #     "table": "proj_bead.acs_wide_tr",
-    #     "api_columns": "geoid as geoid_tr, geoid_bl, year, total_population, total_households, total_housing_units, hh_w_computer, hh_w_smartphone_only, hh_wo_device, hh_using_broadband, share_w_computer, share_w_smartphone_only, share_wo_device, broadband_usage",
+    #     "api_columns": "geoid as geoid_tr, geoid_bl, year, total_population, total_households, total_housing_units, "
+    #                    "hh_w_computer, hh_w_smartphone_only, hh_wo_device, hh_using_broadband, share_w_computer, "
+    #                    "share_w_smartphone_only, share_wo_device, broadband_usage",
     #     "params": ["geoid_tr", "geoid_bl"],
     #     "geoid": "geoid_tr",
     #     "geom": None,
@@ -53,7 +57,13 @@ CONFIG = {
     "bfm_award_co": {
         "table": "proj_bead.bfm_award_co_V3",
         "alias": "awards_co",
-        "api_columns": "awards_co.brandname, awards_co.providerid, awards_co.project, awards_co.project_id, awards_co.program_id, awards_co.geoid_co, awards_co.geoid_st, awards_co.build_req, awards_co.loc_plan, awards_co.loc_sup, awards_co.technology, awards_co.maxdown, awards_co.maxup, awards_co.tranche, awards_co.fund_awarded, awards_co.fund_expended, awards_co.fund_loan, awards_co.fund_grant, awards_co.fund_ob, awards_co.project_cost, awards_co.proj_start, awards_co.proj_end, awards_co.tribal_id, awards_co.tribal_location_pct, awards_co.tribal_funding, awards_co.cadence",
+        "api_columns": "awards_co.brandname, awards_co.providerid, awards_co.project, awards_co.project_id, "
+                       "awards_co.program_id, awards_co.geoid_co, awards_co.geoid_st, awards_co.build_req, "
+                       "awards_co.loc_plan, awards_co.loc_sup, awards_co.technology, awards_co.maxdown, "
+                       "awards_co.maxup, awards_co.tranche, awards_co.fund_awarded, awards_co.fund_expended, "
+                       "awards_co.fund_loan, awards_co.fund_grant, awards_co.fund_ob, awards_co.project_cost, "
+                       "awards_co.proj_start, awards_co.proj_end, awards_co.tribal_id, awards_co.tribal_location_pct, "
+                       "awards_co.tribal_funding, awards_co.cadence",
         "params": [ "geoid_bl", "geoid_co" ],
         "geoid": "geoid_co",
         "geom": None,
@@ -61,8 +71,11 @@ CONFIG = {
         "id": None
     },
     "bead_bl": {
-        "table": "proj_bead.bead_blockv1b",
-        "api_columns": "geoid_bl, geoid_tr, geoid_co, geoid_st, bead_category, bl_25_3_area, bl_100_20_area, cnt_25_3, cnt_100_20, cnt_total_locations, cnt_isp, combo_isp_id, isp_id, pct_served, has_previous_funding, has_copperwire, has_coaxial_cable, has_fiber, has_wireless, only_water_flag, geom",
+        "table": "proj_bead.bead_block_v3",
+        "api_columns": "geoid_bl, geoid_tr, geoid_co, geoid_st, bead_category, bl_25_3_area, bl_100_20_area, "
+                       "cnt_25_3, cnt_100_20, cnt_total_locations, cnt_isp, combo_isp_id, isp_id, pct_served, "
+                       "has_previous_funding, has_copperwire, has_coaxial_cable, has_fiber, has_wireless, "
+                       "only_water_flag, geom",
         "params": ["geoid_bl", "geoid_tr"],
         "geoid": "geoid_bl",
         "geom": "geom",
@@ -74,7 +87,8 @@ CONFIG = {
     },
     "isp_id_to_combo_isp_id": {
         "table": "proj_bead.isp_id_to_combo_isp_id_v1",
-        "api_columns": "isp_id::int, new_alias, array_to_string(ARRAY_AGG(agg_isp_id),',') as agg_isp_id, array_to_string(ARRAY_AGG(combo_isp_id),',') as combo_isp_id",
+        "api_columns": "isp_id::int, new_alias, array_to_string(ARRAY_AGG(agg_isp_id),',') as agg_isp_id, "
+                       "array_to_string(ARRAY_AGG(combo_isp_id),',') as combo_isp_id",
         "params": ["isp_id"],
         "geoid": None,
         "geom": None,
@@ -94,7 +108,10 @@ CONFIG = {
     # "rdof_bl": {
     #     "table": "proj_bead.rdof_bl",
     #     "alias": "rdof",
-    #     "api_columns": "rdof.applicant, rdof.winning_bi, rdof.state, county, rdof.geoid_bl as geoid_bl_2010, rdof.da_numbers, rdof.geoid_co, rdof.tier, rdof.latency, rdof.frn, rdof.sac, rdof.winning_bidder, rdof.winning_bid_total_in_state, rdof.number_of_locations_in_state, rdof.authorized, rdof.\"default\", rdof.version",
+    #     "api_columns": "rdof.applicant, rdof.winning_bi, rdof.state, county, rdof.geoid_bl as geoid_bl_2010, "
+    #                    "rdof.da_numbers, rdof.geoid_co, rdof.tier, rdof.latency, rdof.frn, rdof.sac, "
+    #                    "rdof.winning_bidder, rdof.winning_bid_total_in_state, rdof.number_of_locations_in_state, "
+    #                    "rdof.authorized, rdof.\"default\", rdof.version",
     #     "params": ["applicant", "geoid_bl", "geoid_co", "version"],
     #     "geoid": "geoid_bl",
     #     "geom": None,
